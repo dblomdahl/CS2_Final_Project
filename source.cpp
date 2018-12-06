7232743b94b9d10440e0e602d9601b463cd475fb
Daniel Blomdahl <danielblomdahl@daniels-air.whitworth.edu>
2018-12-06 13:42:33 -0800

    switch .h and .cpp added,

--- source.cpp
+++ source.cpp
@@ -8,14 +8,10 @@
 #include <iostream>
 using namespace std;
 
-// Test to see if gitbub works
-// Another test
-
 int main(){
         char addTrip = 'y';
         int tripSwitch = 0;
         vector<BikingTrip> bikeTrips;
-
 
         do{
             cout << "Add a trip? y/n" << endl;
@@ -33,6 +29,9 @@ int main(){
         case 2: cout << "Choice is backpacking" << endl; 
             break; 
         case 3: cout << "Choice is biking" << endl;
+
+            bikeTrips.push_back( BikingTrip(4,"Whistler", true));
+
             break; 
         case 4: cout << "Choice is climbing" << endl;
             break;
@@ -48,6 +47,10 @@ int main(){
         bikeTrips.push_back( BikingTrip(1,"Silver Mountain", true));
 
         cout << "Days: " << bikeTrips[0].GetDays() << endl;
+        cout << "Days: " << bikeTrips[0].GetLocation() << endl;
+        cout << "Days: " << bikeTrips[1].GetDays() << endl;
+        cout << "Days: " << bikeTrips[1].GetLocation() << endl;
+ 
 
         // BikingTrip b1(1,"Silver Mountain", true);
         // BikingTrip b2(4,"Whistler", true);
@@ -59,15 +62,15 @@ int main(){
         // BikingTrip b3(1,"Beacon Hill", false);
         // BikingTrip b4(4, "Priest Lake", false);
 
-//         cout << b1.GetLocation() << endl;
-//         b1.CalculateRating();
-//         cout << b1.GetRatingString() << endl;
-//         cout << b1.GetRatingValue() << endl;
-// cout << endl;
-//         cout << b2.GetLocation() << endl;
-//         b2.CalculateRating();
-//         cout << b2.GetRatingString() << endl;
-//         cout << b2.GetRatingValue() << endl;
+        cout << bikeTrips[0].GetLocation() << endl;
+        bikeTrips[0].CalculateRating();
+        cout << bikeTrips[0].GetRatingString() << endl;
+        cout << bikeTrips[0].GetRatingValue() << endl;
+        cout << endl;
+        cout << bikeTrips[1].GetLocation() << endl;
+        bikeTrips[1].CalculateRating();
+        cout << bikeTrips[1].GetRatingString() << endl;
+        cout << bikeTrips[1].GetRatingValue() << endl;
 // cout << endl;
 //         cout << b3.GetLocation() << endl;
 //         b3.CalculateRating();
---
+++ switch.h
@@ -0,0 +1,6 @@
+#ifndef _SWITCH_H_
+#define _SWITCH_H_
+
+
+
+#endif
